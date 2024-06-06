567cd1d6ae41349266129c8d3c2acb7311b51c4a
Hablu Programmer <habluprogrammer@gmail.com>
2024-06-06 22:43:40 +0600

    advance js

---
+++ Chapter 1/let.js
@@ -0,0 +1,19 @@
+x = 5; 
+var x;  // var use hoisting
+
+console.log(x)
+
+
+let a = 10; 
+  a = 20; // reassign 
+
+console.log(a)
+
+// const = reassign, re declear, hoisting
+
+const b = 10; // first time 
+
+console.log(b)
+
+
+
